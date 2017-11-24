01ddca60c86dc9ff848ef4bbc7e6a84e74b2f4ac
Berk Öngay <cberkongay@gmail.com>
2017-11-24 15:29:17 +0300

    Book Details and Added New Book

--- BookService/Scripts/app.js
+++ BookService/Scripts/app.js
@@ -2,8 +2,18 @@
     var self = this;
     self.books = ko.observableArray();
     self.error = ko.observable();
+    self.detail = ko.observable();
+    self.authors = ko.observableArray();
+    self.newBook = {
+        Author: ko.observable(),
+        Genre: ko.observable(),
+        Price: ko.observable(),
+        Title: ko.observable(),
+        Year: ko.observable()
+    }
 
-    var booksUrl = '/api/books';
+    var booksUrl = '/api/books/';
+    var authorsUrl = '/api/authors/';
 
     function ajaxHelper(url, method, data) {
         self.error('');
@@ -24,7 +34,35 @@
         });
     }
 
+    self.getBookDetail = function (id) {
+        ajaxHelper(booksUrl + id.Id, 'GET').done(function (data) {
+            self.detail(data);
+        });
+    }
+
+    function getAuthors(){
+        ajaxHelper(authorsUrl, 'GET').done(function (data) {
+            self.authors(data);
+        });
+    }
+
+    self.addBook = function (formElement) {
+        var book = {
+            AuthorId: self.newBook.Author().Id,
+            Title: self.newBook.Title(),
+            Year: self.newBook.Year(),
+            Genre: self.newBook.Genre(),
+            Price: self.newBook.Price(),
+        };
+
+        ajaxHelper(booksUrl, 'POST', book).done(function (id) {
+            self.books.push(id);
+        });
+    }
+
+    getAuthors();
     getBooks();
+    
 };
 
 ko.applyBindings(new ViewModel());
